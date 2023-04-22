81c1dece6f81450bdbb55f887448389c636cfb0e
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-22 13:53:29 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -14,6 +14,10 @@ local webhooks = loadstring(game:HttpGet("https://raw.githubusercontent.com/Reti
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/RBXScripts-Drive/main/girgmnijnrw09LIB.lua"))()
 local functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RetiiAyo/TDS-Player/main/storage/functions.lua"))()
 
+local RS = game:WaitForChild('ReplicatedStorage')
+local RSRF = RS:WaitForChild("RemoteFunction")
+local RSRE = RS:WaitForChild("RemoteEvent")
+
 functions:Initialize()
 
 if multiplayer and typeof(multiplayer) == "table" then
@@ -50,6 +54,48 @@ if getgenv().Settings.Multiplayer.Enabled == true then
 			["content"] = "Online: "..PlrData["Players"].."; Multiplayer Players: "..PlrData["MultiplayerPlayers"]
 		})
 	end
+	
+	TDSWindow = library:CreateWindow("TDS-Player")
+	TDSWindow:Section("Record: ")
+	TDSWindow:Section("")
+        TDSWindow:Section("TimePassed")
+	
+	spawn(function()
+        function TimeConverter(v)
+            if v <= 9 then
+                local conv = "0"..v
+                return conv
+            else
+                return v
+            end
+        end
+        local labelx = nil
+        repeat
+        for i,v in pairs(game.CoreGui:GetDescendants()) do
+        if v:IsA("TextLabel") and v.Text == "TimePassed" then
+            labelx = v
+        end
+        end
+        task.wait()
+        until labelx
+        local startTime = os.time()
+        while task.wait(0.1) do
+        local t = os.time() - startTime
+        local seconds = t % 60
+        local minutes = math.floor(t / 60) % 60
+        labelx.Text = "Time Passed : "..TimeConverter(minutes)..":"..TimeConverter(seconds)
+        getgenv().TimePassed = "Time Passed : "..TimeConverter(minutes)..":"..TimeConverter(seconds)
+        end
+        end)
+	
+	TDSWindow:Section("")
+	TDSWindow:Button("Sell All Farms", function()
+		for i,v in pairs(game.Workspace.Towers:GetChildren()) do
+                if getTroopType(v) == "Farm" and v.Owner.Value == game.Players.LocalPlayer.UserId then
+		    RSRF:InvokeServer("Troops","Sell",{["Troop"] = v,["Recorder"] = true})
+                    task.wait()
+            end
+        end
 	
 else
 	
