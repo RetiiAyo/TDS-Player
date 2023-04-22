c34749df3b4b9a245be04f9715a64627e832dfa8
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-22 12:52:04 +0200

    Update webhooks.lua

--- storage/webhooks.lua
+++ storage/webhooks.lua
@@ -1,8 +1,14 @@
 local module = {}
 
 function module:SendWebhook(Webhook, Data)
-  syn.request({ Url = Webhook, Body = game:GetService("HttpService"):JSONEncode(Data), Method = "POST", Headers = {["content-type"] = "application/json"} })
-  print(FullData[1])
+  syn.request({
+      Url = tostring(Webhook),
+      Body = game:GetService("HttpService"):JSONEncode(Data),
+      Method = "POST",
+      Headers = {
+        ["content-type"] = "application/json"
+      }
+  })
 end
 
 return module
