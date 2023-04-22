461048ecb9808ef0364930d52b84fd0485be7d54
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-22 12:57:01 +0200

    Update webhooks.lua

--- storage/webhooks.lua
+++ storage/webhooks.lua
@@ -1,6 +1,7 @@
 local module = {}
 
 function module:SendWebhook(Webhook, Data)
+  print("Sending webhook")
   syn.request({
       Url = tostring(Webhook),
       Body = game:GetService("HttpService"):JSONEncode(Data),
