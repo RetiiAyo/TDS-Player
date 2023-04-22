9e57f0aa02e553cff1c9c24751f0d3e4af53b172
Retii <109904771+RetiiAyo@users.noreply.github.com>
2023-04-22 12:26:35 +0200

    Update webhooks.lua

--- storage/webhooks.lua
+++ storage/webhooks.lua
@@ -2,11 +2,14 @@ local module = {}
 
 function module:SendWebhook(Data)
   if getgenv().Settings.Logs ~= "WEBHOOK HERE (NOT NEEDED)" then
+    print("Web1")
     local Url = getgenv().Settings.Logs
     local Headers = {["content-type"] = "application/json"}
     local Request = http_request or request or HttpPost or syn.request
     local FullData = {Url = Url, Body = Data, Method = "POST", Headers = Headers}
     Data = game:GetService("HttpService"):JSONEncode(Data)
+    
+    print("Web2")
     
     Request(FullData)
     print(FullData)
